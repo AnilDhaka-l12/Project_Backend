b3a3eaeb2216103520d6cf10789f7e75595f8254
pravijupreti <opporag@gmail.com>
2026-04-27 19:35:21 +0300

    Cors error check

--- Extensions/CorsRegistration.cs
+++ Extensions/CorsRegistration.cs
@@ -2,16 +2,31 @@ namespace ProjectBackend.Extensions;
 
 public static class CorsRegistration
 {
-    /// <summary>
-    /// Registers CORS policy for React frontend
-    /// </summary>
     public static IServiceCollection RegisterCors(this IServiceCollection services)
     {
+        var serviceProvider = services.BuildServiceProvider();
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+        var frontendUrl = configuration["Cors:FrontendUrl"];
+        var adminUrl = configuration["Cors:AdminUrl"];
+        var swaggerUrl = configuration["Cors:SwaggerURL"];
+
+        var origins = new List<string>();
+
+        if (!string.IsNullOrWhiteSpace(frontendUrl))
+            origins.Add(frontendUrl);
+
+        if (!string.IsNullOrWhiteSpace(adminUrl))
+            origins.Add(adminUrl);
+
+        if (!string.IsNullOrWhiteSpace(swaggerUrl))
+            origins.Add(swaggerUrl);
+
         services.AddCors(options =>
         {
             options.AddPolicy("ReactApp", policy =>
             {
-                policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+                policy.WithOrigins(origins.ToArray())
                       .AllowAnyMethod()
                       .AllowAnyHeader()
                       .AllowCredentials();
